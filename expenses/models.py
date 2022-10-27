0e8e16eaea3a8dd98cffe9d01e2a135435f05a65
PythonTattzee <jl##^%hdolD@74>
2022-10-27 23:06:01 +0200

    Ordered the data by date in the models.

--- expenses/models.py
+++ expenses/models.py
@@ -14,7 +14,7 @@ class Category(models.Model):
 
 class Expense(models.Model):
     class Meta:
-        ordering = ('-amount', '-pk')
+        ordering = ('-date', '-pk')
 
     category = models.ForeignKey(Category, models.PROTECT, null=True, blank=True)
 
